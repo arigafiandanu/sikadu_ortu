50c219e29d4409dfa8beed308d2f06cf28a4b795
arigafiandanu <aregavian@gmail.com>
2023-01-21 02:25:22 +0700

    first commit

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,62 @@
+import 'package:get/get.dart';
+
+import '../modules/chat/bindings/chat_binding.dart';
+import '../modules/chat/views/chat_view.dart';
+import '../modules/dashboard/bindings/dashboard_binding.dart';
+import '../modules/dashboard/views/dashboard_view.dart';
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+import '../modules/listChat/bindings/list_chat_binding.dart';
+import '../modules/listChat/views/list_chat_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
+import '../modules/lupaPass/bindings/lupa_pass_binding.dart';
+import '../modules/lupaPass/views/lupa_pass_view.dart';
+import '../modules/profil/bindings/profil_binding.dart';
+import '../modules/profil/views/profil_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.DASHBOARD,
+      page: () => const DashboardView(),
+      binding: DashboardBinding(),
+    ),
+    GetPage(
+      name: _Paths.CHAT,
+      page: () => ChatView(),
+      binding: ChatBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFIL,
+      page: () => const ProfilView(),
+      binding: ProfilBinding(),
+    ),
+    GetPage(
+      name: _Paths.LIST_CHAT,
+      page: () => const ListChatView(),
+      binding: ListChatBinding(),
+    ),
+    GetPage(
+      name: _Paths.LUPA_PASS,
+      page: () => const LupaPassView(),
+      binding: LupaPassBinding(),
+    ),
+  ];
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,24 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
+  static const DASHBOARD = _Paths.DASHBOARD;
+  static const CHAT = _Paths.CHAT;
+  static const PROFIL = _Paths.PROFIL;
+  static const LIST_CHAT = _Paths.LIST_CHAT;
+  static const LUPA_PASS = _Paths.LUPA_PASS;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const LOGIN = '/login';
+  static const DASHBOARD = '/dashboard';
+  static const CHAT = '/chat';
+  static const PROFIL = '/profil';
+  static const LIST_CHAT = '/list-chat';
+  static const LUPA_PASS = '/lupa-pass';
+}
